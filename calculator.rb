4cf02bef3118844f7b4207598b8447f90a2ff665
Chaim Bryski <luzybry94@gmail.com>
2020-04-20 22:52:53 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,3 +2,6 @@
 first_number = 7.6
 second_number = 13
 sum = first_number + second_number
+difference = second_number - first_number
+product = second_number * first_number
+quotient = second_number / first_number
